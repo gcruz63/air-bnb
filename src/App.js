1705eada22aeae52871dede41b23a78ca6a58565
gcruz.63 <gcruzthethird@gmail.com>
2022-04-07 20:25:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Home from './Home';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Air BnB clone</h1>
+      <Header />
+      <Home />
+      { /*HOME */}
+      {/* HEADER */}
+
+      {/* BANNER */}
+      {/*SEARCH */}
+
+      {/* CARDS*/}
+
+      {/* FOOTER*/}
+
+      {/*SEARCH PAGE*/}
+
+    </div>
+  );
+}
+
+export default App;
