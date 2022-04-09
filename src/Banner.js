98babf92715239514e01b6cdde362968fd653d69
gcruz.63 <gcruzthethird@gmail.com>
2022-04-08 20:58:24 -0500

    search date added

--- src/Banner.js
+++ src/Banner.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
 import './Banner.css'
 import { Button } from "@material-ui/core"
-
+import Search from './Search';
 function Banner() {
-    const { showSearch, setShowSearch } = useState(false);
+    const [showSearch, setShowSearch] = useState(false);
 
 
     return (
         <div className="banner">
             <div className="banner__search">
-                {showSearch && <h1>show date</h1>}
-                <Button onclick={() => setShowSearch(!showSearch)}
+                {showSearch && <Search />}
+
+                <Button onClick={() =>
+                    setShowSearch(!showSearch)}
                     className="banner__searchButton"
-                    variant='outlined'>Search Dates</Button>
+                    variant='outlined'>Search Dates
+                </Button>
             </div>
             <div className="banner__info">
                 <h1>Get out and stretch your imagination</h1>
