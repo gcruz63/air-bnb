b122d9efdfd16a888a977d940ca17f1aa4276f10
gcruz.63 <gcruzthethird@gmail.com>
2022-04-12 18:32:52 -0500

    can now add how many guests are staying

--- src/Search.js
+++ src/Search.js
@@ -5,6 +5,9 @@ import "react-date-range/dist/styles.css";
 // theme css file
 import "react-date-range/dist/theme/default.css";
 import { DateRangePicker } from "react-date-range";
+import { Button } from "@material-ui/core";
+import PeopleIcon from "@material-ui/icons/People";
+
 // Date Picker
 function Search() {
     const [startDate, setStartDate] = useState(new Date());
@@ -26,6 +29,15 @@ function Search() {
             <DateRangePicker ranges={
                 [selectionRange]}
                 onChange={handleSelect} />
+            <h2>
+                Number Of Guests
+                <PeopleIcon />
+            </h2>
+            <input min={0}
+                defaultValue={2}
+                type="number" />
+            <Button>Search
+                AirBnb</Button>
         </div>
     )
 }
